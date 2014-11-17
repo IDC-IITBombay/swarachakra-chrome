fb925db48ef6efab09200ad30838454d8359e316
raghunayyar <me@iraghu.com>
2014-11-17 17:43:28 +0530

    Enhancement: Basic Script to keep track of Inputs in DOM.

--- js/config/contentscript.js
+++ js/config/contentscript.js
@@ -1,1 +1,18 @@
-console.log('I am in background');
+/*
+//  This file should be responsible
+// for tracking all inputs on a page.
+//  and then triggering an event on
+//  focus, unfocus that calls / hides the keyboard
+*/
+
+
+$(document).ready(function() {
+  $('body').attr('ng-app', 'Swarachakra');
+
+  var totalinputs = 0; // initialise
+  $("input[type='text']").each( function(index) {
+     totalinputs = totalinputs + 1;
+     console.log(totalinputs);
+  });
+  console.log(totalinputs);
+});
